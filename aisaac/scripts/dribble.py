badd5ff8b6d99c23d830b843d8610524e8417571
Hiroki Sawa <hsawa0709@gmial.com>
2019-05-06 21:34:36 +0900

    Entityのクラス継承、よく使う関数をFunctionからimport、他微調整、dribbleクラスは編集中

---
+++ aisaac/scripts/dribble.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env  python
+# coding:utf-8
+import itertools
+import math
+import rospy
+import numpy as np
+from consai_msgs.msg import Pose
+from consai_msgs.msg import robot_commands
+#from aisaac.srv import Kick
+from aisaac.msg import Status
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Quaternion
+import tf
+import entity
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import functions
+from robot_functions import RobotPid
+ROBOT_LOOP_RATE = 60.
+
+
+class dribble(RobotPid):
+    def __init__(self, robot_params, ball_params, cmd, command_pub):
+        self.robot_params = robot_params
+        self.ball_params = ball_params
+        self.cmd = cmd
+        self.command_pub = command_pub
+
+        self.goal_pos_init_flag = True
+
+        self.pid_circle_center_x = 0.
+        self.pid_circle_center_y = 0.
+
+        self.recursion_max = 10.
+        self.recursion_count = 0.
+
+    def pid_linear(self, goal_pos_x, goal_pos_y, goal_pos_theta):
+        self.Kpv = 2.2
+        self.Kpr = 4.
+        self.Kdv = 5.
+        self.Kdr = 3.
+
+        if self.goal_pos_init_flag == True:
+            self.recursion_count = 0
+            self.next_pos_x, self.next_pos_y = self.pass_plan(goal_pos_x, goal_pos_y)
+
+        d_x = self.next_pos_x - self.robot_params.current_x
+        d_y = self.next_pos_y - self.robot_params.current_y
+        d_theta = goal_pos_theta - self.robot_params.current_theta
+        if self.goal_pos_init_flag:
+            self.goal_pos_init_flag = False
+            self.pid_d_x = 0.
+            self.pid_d_y = 0.
+            self.pid_d_theta = 0.
+            self.pid_p_prev_x = d_x
+            self.pid_p_prev_y = d_y
+            self.pid_p_prev_theta = d_theta
+        else:
+            self.pid_d_x = d_x - self.pid_p_prev_x
+            self.pid_d_y = d_y - self.pid_p_prev_y
+            self.pid_d_theta = d_theta - self.pid_p_prev_theta
+
+            self.pid_p_prev_x = d_x
+            self.pid_p_prev_y = d_y
+            self.pid_p_prev_theta = d_theta
+
+        self.pid_p_x = d_x
+        self.pid_p_y = d_y
+        self.pid_p_theta = d_theta
+
+        Vx = self.Kpv * self.pid_p_x + self.Kdv * self.pid_d_x / (1./ROBOT_LOOP_RATE)
+        Vy = self.Kpv * self.pid_p_y + self.Kdv * self.pid_d_y / (1./ROBOT_LOOP_RATE)
+        Vr = self.Kpr * self.pid_p_theta + self.Kdr * self.pid_d_theta / (1./ROBOT_LOOP_RATE)
+
+        self.cmd.vel_surge = Vx*math.cos(self.robot_params.current_theta)+Vy*math.sin(self.robot_params.current_theta)
+        self.cmd.vel_sway = -Vx*math.sin(self.robot_params.current_theta)+Vy*math.cos(self.robot_params.current_theta)
+        self.cmd.omega = Vr
+        self.command_pub.publish(self.cmd)
+
+    def pid_circle(self, center_x, center_y, x, y, theta):
+        self.Kpv = 2
+        self.Kpr = 7
+        self.Kdr = 4
+        d_x = center_x - self.robot_params.current_x
+        d_y = center_y - self.robot_params.current_y
+        d_theta = theta - self.robot_params.current_theta
+        if self.goal_pos_init_flag:
+            self.goal_pos_init_flag = False
+            self.pid_d_theta = d_theta
+            self.pid_p_prev_theta = d_theta
+        else:
+            self.pid_d_theta = d_theta - self.pid_p_prev_theta
+            self.pid_p_prev_theta = d_theta
+
+        self.pid_p_x = d_x
+        self.pid_p_y = d_y
+        self.pid_p_theta = d_theta
+
+        Vx = 0
+        Vy = 0
+        Vr = 0
+
+        Vx_tangent = 0
+        Vy_tangent = 0
+
+        if math.sqrt(d_x * d_x + d_y * d_y) != 0:
+            Vx = self.Kpv * self.pid_p_x
+            Vy = self.Kpv * self.pid_p_y
+            Vr = self.Kpr * self.pid_p_theta + self.Kdr * self.pid_d_theta / (1./ROBOT_LOOP_RATE)
+            Vx_tangent = (-d_y / math.sqrt(d_x * d_x + d_y * d_y)) * 2
+            Vy_tangent = (d_x / math.sqrt(d_x * d_x + d_y * d_y)) * 2
+
+            self.cmd.vel_surge=(Vx + Vx_tangent)*math.cos(self.robot_params.current_theta)+(Vy + Vy_tangent)*math.sin(self.robot_params.current_theta)
+            self.cmd.vel_sway=-(Vx + Vx_tangent)*math.sin(self.robot_params.current_theta)+(Vy + Vy_tangent)*math.cos(self.robot_params.current_theta)
+            self.cmd.omega=Vr
+            self.command_pub.publish(self.cmd)
+
+    def replan_timerCallback(self, event):
+        self.goal_pos_init_flag = True
