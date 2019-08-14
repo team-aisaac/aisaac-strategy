8bc3e2165cf426a926264a46a6be866d52b3f4c7
Hiroki Sawa <hsawa0709@gmial.com>
2019-08-14 22:10:23 +0900

    開発中

--- aisaac/scripts/direct_free_attack_strategy_calcurator.py
+++ aisaac/scripts/direct_free_attack_strategy_calcurator.py
@@ -27,6 +27,7 @@ class DirectFreeAttack(IndirectFreeAttack):
 
     def calcurate(self, strategy_context=None):
         # type: (StrategyContext) -> StrategyBase
+
         if self._get_who_has_a_ball() == "free":
             pass
         elif self._get_who_has_a_ball() == "robots":
@@ -36,18 +37,27 @@ class DirectFreeAttack(IndirectFreeAttack):
             self.history_who_has_a_ball.pop(0)
             self.history_who_has_a_ball.append("enemy")
 
+        if : # TODO  ballが味方ペナルティエリアに入ってるか
+
+        elif functions.distance_btw_two_points([ball_x, ball_y], [6., 4.5]) < 0.1 or functions.distance_btw_two_points([ball_x, ball_y], [6., -4.5]) < 0.1:
+            result = self.calculate_corner_kick()
+
         if self.history_who_has_a_ball.count("enemy") > 5:
             strategy_context.update("direct_finish", True, namespace="world_model")
             strategy_context.update("defence_or_attack", False, namespace="world_model")
+
 
         result = self.calculate_1()
         return result
 
     def calculate_goal_kick(self):
         for robot_id in active_robot_ids:
+            status = Status()
             robot = self._objects.get_robot_by_id(robot_id)
             if robot.get_role() == "GK":
-                status.status = "keeper"
+                status.status = "pass_chip"
+                status.pass_target_pos_x = self._objects.get_robot_by_role("LFW").get_current_position()[0]
+                status.pass_target_pos_y = self._objects.get_robot_by_role("LFW").get_current_position()[1]
             elif robot.get_role() == "LDF":
                 status.status = "defence1"
             elif robot.get_role() == "RDF":
@@ -55,16 +65,42 @@ class DirectFreeAttack(IndirectFreeAttack):
             elif robot.get_role() == "LFW":
                 #敵kickerとballの延長線上に移動
                 status.status = "move_linear"
-                if nearest_enemy_id != None:
-                    status.pid_goal_pos_x, status.pid_goal_pos_y = functions.calculate_internal_dividing_point(self._enemy[nearest_enemy_id].get_current_position()[0], self._enemy[nearest_enemy_id].get_current_position()[1], self._ball_params.get_current_position()[0], self._ball_params.get_current_position()[1], functions.distance_btw_two_points(self._enemy[nearest_enemy_id].get_current_position(), self._ball_params.get_current_position()) + 0.55, -0.55)
-                    status.pid_goal_theta = math.atan2( (self._ball_params.get_current_position()[1] - self._robot[3].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[2].get_current_position()[0]) )
+                status.pid_goal_pos_x = 2.0
+                status.pid_goal_pos_y = 1.5
             elif robot.get_role() == "RFW":
-                #フリーで最もゴールに近い敵idを返す
                 status.status = "move_linear"
-                free_enemy_id = self._get_free_enemy_id(4, nearest_enemy_id)
-                status.pid_goal_pos_x = (self._ball_params.get_current_position()[0] + self._enemy[free_enemy_id].get_current_position()[0]) / 2
-                status.pid_goal_pos_y = (self._ball_params.get_current_position()[1] + self._enemy[free_enemy_id].get_current_position()[1]) / 2
-                status.pid_goal_theta = math.atan2( (self._ball_params.get_current_position()[1] - self._robot[4].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[4].get_current_position()[0]) )
+                status.pid_goal_pos_x = 2.0
+                status.pid_goal_pos_y = -1.5
+            else:
+                status.status = "none"
+            self._dynamic_strategy.set_robot_status(robot_id, status)
+
+        result = self._dynamic_strategy
+        return result
+
+    def calculate_corner_kick_left(self):
+        for robot_id in active_robot_ids:
+            status = Status()
+            robot = self._objects.get_robot_by_id(robot_id)
+            if robot.get_role() == "GK":
+                status.status = "pass_chip"
+                status.pass_target_pos_x = self._objects.get_robot_by_role("LFW").get_current_position()[0]
+                status.pass_target_pos_y = self._objects.get_robot_by_role("LFW").get_current_position()[1]
+            elif robot.get_role() == "LDF":
+                status.status = ""
+                # Todo
+
+            elif robot.get_role() == "RDF":
+                status.status = "defence2"
+            elif robot.get_role() == "LFW":
+                #敵kickerとballの延長線上に移動
+                status.status = "move_linear"
+                status.pid_goal_pos_x = 2.0
+                status.pid_goal_pos_y = 1.5
+            elif robot.get_role() == "RFW":
+                status.status = "move_linear"
+                status.pid_goal_pos_x = 2.0
+                status.pid_goal_pos_y = -1.5
             else:
                 status.status = "none"
             self._dynamic_strategy.set_robot_status(robot_id, status)
