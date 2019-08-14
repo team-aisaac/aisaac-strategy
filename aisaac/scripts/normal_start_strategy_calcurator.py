98892287a71bdddc17b02df533aad30c4bc8b0cb
Araki Yugo <y1103orz@yahoo.co.jp>
2019-08-14 10:02:08 +0900

    レフリーに対する行動 (通常防御)

--- aisaac/scripts/normal_start_strategy_calcurator.py
+++ aisaac/scripts/normal_start_strategy_calcurator.py
@@ -155,13 +155,48 @@ class NormalStartStrategyCalcurator(StrategyCalcuratorBase):
         result = self._dynamic_strategy
         return result
 
+
+    def _defence_strategy(self, strategy_context=None):
+        active_robot_ids = self._get_active_robot_ids()
+        active_enemy_ids = self._get_active_enemy_ids()
+        status = Status()
+        nearest_enemy_id = self._objects.get_enemy_ids_sorted_by_distance_to_ball(active_enemy_ids)[0]
+        for idx, robot_id in enumerate(active_robot_ids):
+            if idx == 0:
+                status.status = "keeper"
+            elif idx == 1:
+                status.status = "defence1"
+            elif idx == 2:
+                status.status = "defence2"
+            elif idx == 3:
+                #ballの位置に移動
+                status.status = "move_linear"
+                status.pid_goal_pos_x = self._ball_params.get_current_position()[0]
+                status.pid_goal_pos_y = self._ball_params.get_current_position()[1]
+                status.pid_goal_theta = math.atan2( (self._ball_params.get_current_position()[1] - self._robot[3].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[3].get_current_position()[0]) )
+            elif idx == 4:
+                #フリーで最もゴールに近い敵idを返す
+                status.status = "move_linear"
+                free_enemy_id = self._get_free_enemy_id(4, nearest_enemy_id)
+                status.pid_goal_pos_x = (self._ball_params.get_current_position()[0] + self._enemy[free_enemy_id].get_current_position()[0]) / 2
+                status.pid_goal_pos_y = (self._ball_params.get_current_position()[1] + self._enemy[free_enemy_id].get_current_position()[1]) / 2
+                status.pid_goal_theta = math.atan2( (self._ball_params.get_current_position()[1] - self._robot[4].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[4].get_current_position()[0]) )
+            else:
+                status.status = "none"
+            self._dynamic_strategy.set_robot_status(robot_id, status)
+
+
+        result = self._dynamic_strategy
+        return result
     """
     referee_branchがNORMAL_STARTの場合のCalcurator。
     """
     def calcurate(self, strategy_context=None):
         # type: (StrategyContext) -> StrategyBase
-        if True:
+        if strategy_context.get_last("defence_or_attack", namespace="world_model"):
             result = self._attack_strategy1(strategy_context)
+        else:
+            result = self._defence_strategy(strategy_context)
 
         return result
 
@@ -182,6 +217,7 @@ class NormalStartKickOffStrategyCalcurator(StrategyCalcuratorBase):
 
         if distance < succeeded_area:
             strategy_context.update("kickoff_complete", True, namespace="world_model")
+            strategy_context.update("defence_or_attack", True, namespace="world_model")
 
         self._dynamic_strategy.clone_from(self._static_strategies['initial'])
 
@@ -254,8 +290,15 @@ class NormalStartKickOffDefenceStrategyCalcurator(StrategyCalcuratorBase):
         self._dynamic_strategy = DynamicStrategy()
         self._objects = objects
 
-    def calcurate(self, strategy_context=None):
+    def calcurate(self, strategy_context=None, referee_branch="KICKOFF_DEFENCE"):
         # type: (StrategyContext) -> StrategyBase
+
+        if referee_branch == "NORMAL_START":
+            if self._detect_enemy_kick():
+                strategy_context.update("enemy_kick", True, namespace="world_model")
+                strategy_context.update("defence_or_attack", False, namespace="world_model")
+        else:
+            self._set_placed_ball_position(self._ball_params.get_current_position())
 
         active_robot_ids = self._get_active_robot_ids()
         active_enemy_ids = self._get_active_enemy_ids()
@@ -273,7 +316,7 @@ class NormalStartKickOffDefenceStrategyCalcurator(StrategyCalcuratorBase):
                 status.status = "move_linear"
                 if nearest_enemy_id != None:
                     status.pid_goal_pos_x, status.pid_goal_pos_y = functions.calculate_internal_dividing_point(self._enemy[nearest_enemy_id].get_current_position()[0], self._enemy[nearest_enemy_id].get_current_position()[1], self._ball_params.get_current_position()[0], self._ball_params.get_current_position()[1], functions.distance_btw_two_points(self._enemy[nearest_enemy_id].get_current_position(), self._ball_params.get_current_position()) + 0.8, -0.8)
-                    status.pid_goal_theta = math.atan2((self._ball_params.get_current_position()[1] - self._robot[3].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[2].get_current_position()[0]))
+                    status.pid_goal_theta = math.atan2((self._ball_params.get_current_position()[1] - self._robot[3].get_current_position()[1]) , (self._ball_params.get_current_position()[0] - self._robot[3].get_current_position()[0]))
                     if status.pid_goal_pos_x >= -0.2 and status.pid_goal_pos_y >= 0:
                         status.pid_goal_pos_x = -0.2
                         status.pid_goal_pos_y = 0.8
