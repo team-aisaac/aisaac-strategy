83636aa9c44cc11278485d57ddd5e899325a7951
ishitaku5522 <ishitaku5522@gmail.com>
2019-08-15 09:52:08 +0900

    penalty shoot

--- aisaac/scripts/robot.py
+++ aisaac/scripts/robot.py
@@ -147,6 +147,15 @@ class Robot(object):
             elif self.status.robot_status == "prepare_shoot":
                 self.kick.shoot_ball(should_wait=True)
 
+            elif self.status.robot_status == "penalty_shoot":
+                self.kick.shoot_ball(ignore_penalty_area=True)
+            elif self.status.robot_status == "penalty_shoot_right":
+                self.kick.shoot_ball(target="right", ignore_penalty_area=True)
+            elif self.status.robot_status == "penalty_shoot_left":
+                self.kick.shoot_ball(target="left", ignore_penalty_area=True)
+            elif self.status.robot_status == "penalty_shoot_center":
+                self.kick.shoot_ball(target="center", ignore_penalty_area=True)
+
             elif self.status.robot_status == "receive":
                 self.kick.receive_ball(self.ctrld_robot.get_future_position()[0],
                                        self.ctrld_robot.get_future_position()[1])
