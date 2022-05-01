6defc03e1ced0eb4f46945639b95fdf44cc5bb41
kazurs <kwbrtpymt+git@gmail.com>
2022-05-01 17:56:10 +0900

    指令値送信処理の変更 - SSL Visionで取得したロボット位置(x, y座標)を追加 - 指令値駆動からSSL Vision駆動で動くように変更

--- aisaac/src/sender_xbee_yellow.cpp
+++ aisaac/src/sender_xbee_yellow.cpp
@@ -41,25 +41,48 @@ class TopicRxHandler {
 public:
     TopicRxHandler() : sequenceNumber(0) {}
     void command_callback(const consai_msgs::robot_commandsConstPtr& msg) {
-        aisaac::commandToRobot commands;
+        int robotCommandCoordinateSystemType = 0;
 
-        int32_t x_tmp = int32_t(msg->vel_surge * 1000);
-        int32_t y_tmp = int32_t(msg->vel_sway * 1000);
-        float vector_sum = pow(x_tmp,2) + pow(y_tmp,2); 
-        if(sqrt(vector_sum) > VEL_MAX){
-            float k = sqrt(vector_sum) / VEL_MAX;
-            x_tmp = x_tmp / k;
-            y_tmp = y_tmp / k;
+        int32_t x_tmp = int32_t(msg->vel_surge * 1000); // m -> mm
+        int32_t y_tmp = int32_t(msg->vel_sway * 1000);  // m -> mm
+        if (robotCommandCoordinateSystemType == 0) {
+            _commands.robotCommandCoordinateSystemType = 0; // Vector
+            float vector_sum = pow(x_tmp,2) + pow(y_tmp,2); 
+            if(sqrt(vector_sum) > VEL_MAX){
+                float k = sqrt(vector_sum) / VEL_MAX;
+                x_tmp = x_tmp / k;
+                y_tmp = y_tmp / k;
+            }
+        } else if (robotCommandCoordinateSystemType == 1) {
+            _commands.robotCommandCoordinateSystemType = 1; // World coordinate system
         }
-        commands.x_vector = (int16_t)x_tmp;
-        commands.y_vector = (int16_t)y_tmp;
-        
-        commands.angleTypeSelect = 1;   // omega 
-        commands.angle = (uint16_t)int16_t(msg->omega * (180.0 / M_PI) * 10);
+        _commands.x_vector = (int16_t)x_tmp;
+        _commands.y_vector = (int16_t)y_tmp;
 
-        commands.calibrationValid = 0;
-        commands.calibrationXPosition = 0;
-        commands.calibrationYPosition = 0;
+        _commands.angleTypeSelect = 1;   // omega 
+        _commands.angle = (uint16_t)int16_t(msg->omega * (180.0 / M_PI) * 10);
+
+        if(msg->kick_power > 0){
+            _commands.kickParameter.sensorUse = 1;   // kick_flag
+            _commands.kickParameter.kickType = 0;    // kick_chip_flag
+            _commands.kickParameter.kickStrength = 0;
+        }else{
+            _commands.kickParameter.sensorUse = 0;   // kick_flag
+            _commands.kickParameter.kickType = 0;    // kick_chip_flag
+            _commands.kickParameter.kickStrength = 0;
+        }
+    }
+    void odom_callback(const nav_msgs::Odometry& msg) {
+        float x_position, y_position;
+        x_position = msg->pose.pose.position.x;
+        y_position = msg->pose.pose.position.y;
+        // Calculate orientation of robot (at world coordinate system)
+        tf::Quaternion quat(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w);
+        tf::Matrix3x3(quat).getRPY(current_orientation[0], current_orientation[1], current_orientation[2]);
+
+        _commands.calibrationValid = 1; // 1: Valid
+        _commands.calibrationXPosition = (int16_t)(x_position * 1000);  // m -> mm
+        _commands.calibrationYPosition = (int16_t)(y_position * 1000);  // m -> mm
 
         double current_orientation_deg = current_orientation[2] * (180 / M_PI);
         while(!(0 <= current_orientation_deg && current_orientation_deg < 360)){
@@ -69,27 +92,14 @@ public:
                 current_orientation_deg -= 360;
             }
         }
-        commands.calibrationAngle = (uint16_t)uint16_t(current_orientation_deg);
+        _commands.calibrationAngle = (uint16_t)current_orientation_deg;
 
-        if(msg->kick_power > 0){
-            commands.kickParameter.sensorUse = 1;   // kick_flag
-            commands.kickParameter.kickType = 0;    // kick_chip_flag
-            commands.kickParameter.kickStrength = 0;
-        }else{
-            commands.kickParameter.sensorUse = 0;   // kick_flag
-            commands.kickParameter.kickType = 0;    // kick_chip_flag
-            commands.kickParameter.kickStrength = 0;
-        }
+        _commands.miscByte = 0;
 
-        commands.miscByte = 0;
-
+        // Send command message
         std::vector<unsigned char> commandvec;
         sender->generateFT4(commands, commandvec);
         sender->sendCommand(robotID, sequenceNumber++, commandvec);
-    }
-    void odom_callback(const nav_msgs::Odometry& msg) {
-        tf::Quaternion quat(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w);
-        tf::Matrix3x3(quat).getRPY(current_orientation[0], current_orientation[1], current_orientation[2]);
     }
     void shutdown_callback(const aisaac::Shutdown& msg) {
         // if(msg.shutdown_flag == true){
@@ -102,6 +112,7 @@ public:
     }
     int robotID = 0;
 private:
+    aisaac::commandToRobot _commands;
     double current_orientation[3] = {0, 0, 0};
     unsigned char sequenceNumber;
     aisaac::Sender *sender;
