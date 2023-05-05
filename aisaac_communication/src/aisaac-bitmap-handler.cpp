3db4af92f2f783c8d84e6a9cf72044d52dd2b9c0
kazurs <kwbrtpymt+git@gmail.com>
2023-05-06 03:04:24 +0900

    Modification of variable units

--- aisaac_communication/src/aisaac-bitmap-handler.cpp
+++ aisaac_communication/src/aisaac-bitmap-handler.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <functional>
 #include <cstdlib>
+#include <cmath>
 
 namespace aisaac
 {
@@ -93,9 +94,9 @@ namespace aisaac
 
         // goal_pose
         aisaacpb::Position goal_pose;
-        goal_pose.set_x((int32_t)msg->goal_pose.x);
-        goal_pose.set_y((int32_t)msg->goal_pose.y);
-        goal_pose.set_theta((int32_t)msg->goal_pose.theta);
+        goal_pose.set_x((int32_t)(msg->goal_pose.x * 1000.0));  // m -> mm
+        goal_pose.set_y((int32_t)(msg->goal_pose.y * 1000.0));  // m -> mm
+        goal_pose.set_theta((int32_t)(msg->goal_pose.theta * 180.0 / M_PI));    // rad -> deg
         command_to_robot.set_allocated_middle_goal_pose(&goal_pose);
 
         // prohibited_zone_ignore
@@ -106,9 +107,9 @@ namespace aisaac
 
         // middle_goal_pose
         aisaacpb::Position middle_goal_pose;
-        middle_goal_pose.set_x((int32_t)msg->middle_goal_pose.x);
-        middle_goal_pose.set_y((int32_t)msg->middle_goal_pose.y);
-        middle_goal_pose.set_theta((int32_t)msg->middle_goal_pose.theta);
+        middle_goal_pose.set_x((int32_t)(msg->middle_goal_pose.x * 1000.0));    // m -> mm
+        middle_goal_pose.set_y((int32_t)(msg->middle_goal_pose.y * 1000.0));    // m -> mm
+        middle_goal_pose.set_theta((int32_t)(msg->middle_goal_pose.theta * 180.0 / M_PI));  // rad -> deg
         command_to_robot.set_allocated_middle_goal_pose(&middle_goal_pose);
 
         // Dribble
@@ -121,12 +122,12 @@ namespace aisaac
         dribble.set_dribbler_active(msg->dribbler_active);
         // -dribble_goal
         aisaacpb::Position dribble_goal;
-        dribble_goal.set_x((int32_t)msg->dribble_goal.x);
-        dribble_goal.set_y((int32_t)msg->dribble_goal.y);
-        dribble_goal.set_theta((int32_t)msg->dribble_goal.theta);
+        dribble_goal.set_x((int32_t)(msg->dribble_goal.x * 1000.0));    // m -> mm
+        dribble_goal.set_y((int32_t)(msg->dribble_goal.y * 1000.0));    // m -> mm
+        dribble_goal.set_theta((int32_t)(msg->dribble_goal.theta * 180.0 / M_PI));  // rad -> deg
         dribble.set_allocated_dribble_goal(&dribble_goal);
         // -dribble_complete_distance
-        dribble.set_dribble_complete_distance(msg->dribble_complete_distance);
+        dribble.set_dribble_complete_distance(msg->dribble_complete_distance);  // mm
         command_to_robot.set_allocated_dribble(&dribble);
 
         // Kick
@@ -143,13 +144,13 @@ namespace aisaac
         kick.set_free_kick_flag(msg->free_kick_flag);
         // -ball_goal
         aisaacpb::Position ball_goal;
-        ball_goal.set_x((int32_t)msg->ball_goal.x);
-        ball_goal.set_y((int32_t)msg->ball_goal.y);
+        ball_goal.set_x((int32_t)(msg->ball_goal.x * 1000.0));  // m -> mm
+        ball_goal.set_y((int32_t)(msg->ball_goal.y * 1000.0));  // m -> mm
         // msg->ball_goal.vel_x;
         // msg->ball_goal.vel_y;
         kick.set_allocated_ball_goal(&ball_goal);
         // -ball_target_allowable_error
-        kick.set_ball_target_allowable_error(msg->ball_target_allowable_error);
+        kick.set_ball_target_allowable_error(msg->ball_target_allowable_error); // mm
         command_to_robot.set_allocated_kick(&kick);
         
         std::string serialized_str;
